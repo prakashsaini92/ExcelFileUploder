878830fdb8120330db98cf68aa5cba9fd62c6356
Prakash Saini <Prakash@gisdc.doit>
2019-08-30 17:06:09 +0530

    first commit

---
+++ precache-manifest.77052cc042a00eaa6bc33ba2f0ddba22.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d732bf835ab17936f5e60ace45d0dfd6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6bd7f76fdd5aad1ba919",
+    "url": "./static/css/main.9aa65320.chunk.css"
+  },
+  {
+    "revision": "4860557243e8f12086c0",
+    "url": "./static/js/2.bc4debb9.chunk.js"
+  },
+  {
+    "revision": "6bd7f76fdd5aad1ba919",
+    "url": "./static/js/main.aa4388c2.chunk.js"
+  },
+  {
+    "revision": "df306a1b3a9feed5d1c7",
+    "url": "./static/js/runtime~main.e3765b1e.js"
+  }
+]);
